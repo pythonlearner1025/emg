b3f23e597fbc45cc3117593d53fb19cf103dc859
pythonlearner1025 <mjsong2021@gmail.com>
2023-11-26 22:50:22 -0500

    prettify putemg train code

--- putemg.py
+++ putemg.py
@@ -4,14 +4,15 @@ from torch.optim import Adam
 from torch.nn import MSELoss
 from lib.models import TEMG
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
 
-def train():
-  # print columns
-  file = 'out/force.npy'
-  # Splitting the dataset
+# TODO: 
+# - write script to download dataset from putEMG
+
+def train(file):
   data = np.load(file)
   train_size = int(0.8 * len(data))  # 80% of data for training
   val_size = len(data) - train_size  # Remaining 20% for validation
@@ -21,75 +22,68 @@ def train():
   loss = MSELoss()
   optim = Adam(net.parameters())
 
-  # Creating DataLoaders for each set
-  #train_iterator = iterator(np.array(train_dataset), BS=4, shuffle=True)
-
-  epochs = 100
+  epochs = 50
+  train_loss,val_loss = [],[]
   for i in range(epochs):
-    losses = []
+    ts = []
     for X,Y in tqdm(t:=iterator(np.array(train_dataset), BS=4, shuffle=True)):
       optim.zero_grad()
       X,Y = torch.tensor(X,dtype=torch.float32,requires_grad=True), torch.tensor(Y[:,0],dtype=torch.float32)
       o = net(X).squeeze()
       l = loss(o,Y)
-     # print(o)
-     # print(Y)
       l.backward()
       optim.step()
-      losses.append(l.item())
+      t.append(l.item())
+      train_loss.append(l.item())
 
     if i % 4 == 0:
-      ls = []
+      vals = []
       for X,Y in tqdm(t:=iterator(np.array(val_dataset), BS=4, shuffle=False)):
         X,Y = torch.tensor(X,dtype=torch.float32,requires_grad=True), torch.tensor(Y[:,0],dtype=torch.float32)
         o = net(X).squeeze()
         l = loss(o,Y)
-        ls.append(l.item())
-      print(f'EPOCH{i}: {sum(ls)/len(ls):7.2f} avg val loss')
+        vals.append(l.item())
+      print(f'Epoch {i} | {sum(vals)/len(vals):7.2f} val loss')
+      val_loss.append(sum(vals)/len(vals))
+    print(f'Epoch {i} | {sum(ts)/len(ts):7.2f} train loss')
 
-    #print(f'EPOCH {i}, {sum(losses)/len(losses):7.2f} avg loss')
+  plt.plot(list(range(len(train_loss))), train_loss, label='train loss')
+  plt.plot(list(range(len(val_loss))), val_loss, label='val loss')
+  plt.legend()
+  plt.ylabel('MSE Loss')
+  plt.show()
 
-'''
-The data was sampled at 5120 Hz, with 12-bit A/D conversion using 3 Hz high-pass and 900 Hz low-pass filter. 
-All signals were pre-amplified with gain of 5, using amplifiers placed on subject’s arm, resulting in total gain of 200. 
-'''
-def iterator(data,BS=16,shuffle=True):
-  # B,500,9 --> B,X,8,8
+def iterator(data,BS=4,shuffle=True):
   B,C,features = data.shape
-  X = data[:,:,:-2]
-  Y = data[:,:,-2:]
-  X = X[:,:-(C%(features-2)),:]
-  Y = Y[:,:-(C%(features-2))]
+  X,Y = data[:,:,:-2],data[:,:,-2:]
+  X,Y = X[:,:-(C%(features-2)),:],Y[:,:-(C%(features-2))]
   C = (C//(features-2))
   X = X.reshape(B,C,features-2*features-2)
   Y = Y.reshape(B,C,features-2,2)[:,:,0,:]
   assert X.shape[1] == Y.shape[1]
-  
+
   if shuffle:
     indices = np.arange(X.shape[0])
     np.random.shuffle(indices)
     X,Y = X[indices],Y[indices]
 
   for i in range(0, X.shape[0], BS):
-    X_batch = X[i:i + BS]
-    Y_batch = Y[i:i + BS]
+    X_batch,Y_batch = X[i:i+BS],Y[i:i+BS]
     Y_batch = Y_batch[:,:,0]/Y_batch[:,:,1]
     Y_batch[Y_batch<0] = 0
     yield X_batch, Y_batch
 
-def load_putemg_force():
-  f = 'putEMG/Data-HDF5'
-  fs = [os.path.join(f,x) for x in os.listdir(f) if 'mvc' in x]
+def load_putemg_force(in_f,out_f,sr=1280,window=500,overlap=250,avg_last_n=10,adc_bit=10,gain=200):
+  fs = [os.path.join(in_f,x) for x in os.listdir(in_f) if 'mvc' in x]
 
   def adc2uV(x):
-    return x/(2**12)*(10**6/200)
+    return x/(2**12)*(adc_bit**6/gain)
 
   # return N total chunks where N = total_samps // samples_per_window
-  def chunk(buff,window,overlap,sr=1280):
-
+  def chunk(buff):
     # return 2d list of size (samps_per_window,features)
-    def listify(chunk,n=10):
-      fs = [l['force'] for l in chunk[-n:]]
+    def listify(chunk):
+      fs = [l['force'] for l in chunk[-avg_last_n:]]
       f = sum(fs)/len(fs)
       for x in chunk:
         x['force'] = f
@@ -107,8 +101,7 @@ def load_putemg_force():
   emgs = {f'EMG_{i}' for i in list(range(9,17))}
 
   all = []
-  for j,f in enumerate(fs):
-    if j == 10: break
+  for j,f in enumerate(tqdm(t:=fs)):
     frame = pd.read_hdf(f)
     newrows = []
 
@@ -121,13 +114,19 @@ def load_putemg_force():
       newrow['mvc'] = row['FORCE_MVC']
       newrows.append({**newrow,**emgs})
 
-    all.append(np.array(chunk(newrows,500,250)))
+    all.append(np.array(chunk(newrows)))
   res = np.vstack(all)
-  print(res.shape)
+  np.save(out_f,res)
 
-  out = 'out/force.npy'
-  np.save(out,res)
-
+'''
+putEMG dataset info
+  - force data was sampled at 5120 Hz, with 12-bit A/D conversion 
+  using 3 Hz high-pass and 900 Hz low-pass filter. 
+  - All signals were pre-amplified with gain of 5, using amplifiers 
+  placed on subject’s arm, resulting in total gain of 200. 
+'''
 if __name__ == '__main__':
-  #load_putemg_force()
-  train()
+  folder = 'putEMG/Data-HDF5'
+  file = 'out/force.npy'
+  load_putemg_force(folder,file,sr=1280,window=500,overlap=250,avg_last_n=10,adc_bit=10,gain=200)
+  train(file)
